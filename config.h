70da7f548e1ed5e834d3ede92d9475d57f326985
youkwhd <lolywk@tutanota.com>
2022-03-29 19:25:18 +0700

    fix: syntax error and dmenu color flags

--- config.h
+++ config.h
@@ -1,6 +1,6 @@
 /* See LICENSE file for copyright and license details. */
 
-#include "colors/main.h";
+#include "./colors/main.h"
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
@@ -51,7 +51,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
