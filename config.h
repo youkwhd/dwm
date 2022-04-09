015aa61e809deacb7843b7c6c3f942db62ebc676
youkwhd <lolywk@tutanota.com>
2022-04-09 22:24:07 +0700

    patch: revert patch vanity gaps to use the cfact version

--- config.h
+++ config.h
@@ -3,12 +3,15 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static int smartgaps                = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-/* static const char *fonts[]          = { "JetBrains Mono:size=9", "JoyPixels:pixelsize=12:antialias=true:autohint=true" }; */
-/* static const char dmenufont[]       = "JetBrains Mono:size=9"; */
+static const char *fonts[]          = { "JetBrains Mono:size=9", "JoyPixels:pixelsize=12:antialias=true:autohint=true" };
+static const char dmenufont[]       = "JetBrains Mono:size=9";
 
 /* colors variables */
 #include "colors.h"
@@ -32,11 +35,26 @@ static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
+#define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
+#include "vanitygaps.c"
+
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+	{ "[@]",      spiral },
+	{ "[\\]",     dwindle },
+	{ "H[]",      deck },
+	{ "TTT",      bstack },
+	{ "===",      bstackhoriz },
+	{ "HHH",      grid },
+	{ "###",      nrowgrid },
+	{ "---",      horizgrid },
+	{ ":::",      gaplessgrid },
+	{ "|M|",      centeredmaster },
+	{ ">M>",      centeredfloatingmaster },
+	{ NULL,       NULL },
 };
 
 /* key definitions */
@@ -66,6 +84,25 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY|ShiftMask,             XK_n,      setcfact,       {.f = +0.25} },
+	{ MODKEY|ShiftMask,             XK_m,      setcfact,       {.f = -0.25} },
+	{ MODKEY|ShiftMask,             XK_b,      setcfact,       {.f =  0.00} },
+	{ MODKEY|ShiftMask|ControlMask, XK_h,      incrgaps,       {.i = +1 } },
+	{ MODKEY|ShiftMask|ControlMask, XK_l,      incrgaps,       {.i = -1 } },
+	{ MODKEY|ControlMask,           XK_h,      incrigaps,      {.i = +1 } },
+	{ MODKEY|ControlMask,           XK_l,      incrigaps,      {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_h,      incrogaps,      {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_l,      incrogaps,      {.i = -1 } },
+	{ MODKEY,                       XK_y,      incrihgaps,     {.i = +1 } },
+	{ MODKEY,                       XK_o,      incrihgaps,     {.i = -1 } },
+	{ MODKEY|ControlMask,           XK_y,      incrivgaps,     {.i = +1 } },
+	{ MODKEY|ControlMask,           XK_o,      incrivgaps,     {.i = -1 } },
+	{ MODKEY|ControlMask,           XK_i,      incrohgaps,     {.i = +1 } },
+	{ MODKEY|ControlMask,           XK_u,      incrohgaps,     {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_y,      incrovgaps,     {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_o,      incrovgaps,     {.i = -1 } },
+	{ MODKEY,                       XK_0,      togglegaps,     {0} },
+	{ MODKEY|ShiftMask,             XK_0,      defaultgaps,    {0} },
 	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
