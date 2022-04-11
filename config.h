87fe2cd3d2048ef1adc62e02b25dd0a86e180755
youkwhd <lolywk@tutanota.com>
2022-04-12 00:05:44 +0700

    config: use no ligature font

--- config.h
+++ config.h
@@ -10,8 +10,8 @@ static const unsigned int gappov    = 10;       /* vert outer gap between window
 static int smartgaps                = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrains Mono:size=9", "JoyPixels:pixelsize=12:antialias=true:autohint=true" };
-static const char dmenufont[]       = "JetBrains Mono:size=9";
+static const char *fonts[]          = { "JetBrains Mono NL:size=9", "JoyPixels:pixelsize=12:antialias=true:autohint=true" };
+static const char dmenufont[]       = "JetBrains Mono NL:size=9";
 
 /* colors variables */
 #include "colors.h"
